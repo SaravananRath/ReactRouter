69d255b3319c280a6b2f554582c1496184f6505c
SaravananMart <r.saravanan@codingmart.com>
2018-08-29 12:30:55 +0530

    intermediate

--- src/Components/Writers/Writer/index.js
+++ src/Components/Writers/Writer/index.js
@@ -4,6 +4,6 @@ export default ({id,name,born,deceased,description,image}) =>
 <Fragment>
     <img src={image} alt={name} style={{maxWidth:300}}/>
     <h1>{name}</h1>
-    <h3>{born}&mdash;{deceased}</h3>
+    <h3>{born} &mdash; {deceased}</h3>
     <p>{description}</p>
 </Fragment>
